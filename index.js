8ad11e07cc5bc7f9e0b41a7d4ae63ace597e3038
Jeni Dang <jenipdang@yahoo.com>
2021-05-12 19:56:09 -0700

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(ride){
   // Write your code here!
+if (ride <= 400) {
+  return 'This one is on me!';
+}
+else if (ride <= 2499) {
+  return 'I will gladly take your thirty bucks.';
+}
+else {return 'No can do.'}
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(location){
   // Write your code here!
+return ternaryCheckCity = location === "NYC" ? "Ok, sounds good." : "No go.";
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(amount){
   // Write your code here!
+  switch (amount) {
+    case 'generous':
+      return "Thank you so much.";
+    case 'not as generous':
+      return "Thank you.";
+    default:
+      return 'Bye.';
+
+}
 }
